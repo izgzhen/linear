1009d61b501122d793153d9dddbcd9ff244ded74
Zhen Zhang <izgzhen@gmail.com>
2016-01-13 21:46:16 +0800

    clean typechecking error

--- src/TypeCheck.hs
+++ src/TypeCheck.hs
@@ -9,8 +9,8 @@ type EitherS = Either String
 typeCheck :: Context -> Term -> EitherS (Type, Context)
 typeCheck ctx (TmVar x) =
     case M.lookup x ctx of
-        Just (Linear prety) -> Right (Linear prety, M.delete x ctx) -- (A-LVAR)
-        Just (Unres  prety) -> Right (Unres prety,  ctx)            -- (A-UVAR)
+        Just ty@(Type Linear _) -> Right (ty, M.delete x ctx) -- (A-LVAR)
+        Just ty@(Type Unres  _) -> Right (ty, ctx)            -- (A-UVAR)
         Nothing -> Left $ "No such variable: " ++ show x
 
 typeCheck ctx (TmBool q b) = Right (Type q TyBool, ctx)             -- (A-BOOL)
@@ -26,11 +26,12 @@ typeCheck ctx (TmCond t1 t2 t3) = do                                -- (A-IF)
                 else fail $ "Condition branch types don't unify: " ++ show ty2 ++ ", " ++ show ty3
         else fail $ "Wrong type of condition for if: " ++ show prety
 
-typeCheck ctx (TmPair t1 t2) = do                                   -- (A-PAIR)
+typeCheck ctx (TmPair q t1 t2) = do                                   -- (A-PAIR)
     (ty1@(Type q1 _), ctx')  <- typeCheck ctx  t1
     (ty2@(Type q2 _), ctx'') <- typeCheck ctx' t2
-    let q = q1 /\ q2
-    return (Type q (TyPair ty1 ty2), ctx'')
+    if (q <| q1) && (q <| q2) -- containment property
+        then return (Type q (TyPair ty1 ty2), ctx'')
+        else fail $ "Containment is violated in TmPair"
 
 typeCheck ctx (TmSplit t1 x y t2) = do                              -- (A-SPLIT)
     (Type q prety1, ctx') <- typeCheck ctx t1
@@ -42,7 +43,7 @@ typeCheck ctx (TmSplit t1 x y t2) = do                              -- (A-SPLIT)
         _ -> fail $ "Wrong type for splitting: " ++ show prety1
 
 typeCheck ctx (TmAbs q x ty1 t2) = do                               -- (A-ABS)
-    (ctx', ty2) <- typeCheck (M.insert x ty1 ctx) t2
+    (ty2, ctx') <- typeCheck (M.insert x ty1 ctx) t2
     when (q == Unres) $ do
         let (Just ty) = M.lookup x ctx'
         case ty of
@@ -58,13 +59,14 @@ typeCheck ctx (TmApp t1 t2) = do
             if ty11 == ty11'
                 then return (ty12, ctx'')
                 else fail $ "Unmatched type for application: " ++
-                            show ty11 ++ " and " show ty11'
+                            show ty11 ++ " and " ++ show ty11'
         _ -> fail $ "Wrong type for application: " ++ show ty1
 
 -- lin ⊑ un
-(/\) :: Qualifier -> Qualifier -> Qualifier
-(/\) Unres Unres = Unres
-(/\) Linear _    = Linear
+(<|) :: Qualifier -> Qualifier -> Bool
+(<|) Unres Unres = True
+(<|) Linear _    = True
+(<|) _ _         = False
 
 -- (÷)
 ctxdiff :: Context -> Context -> Context
@@ -72,10 +74,10 @@ ctxdiff ctx ctx' = ctxdiff' ctx $ M.toList ctx'
 
 ctxdiff' :: Context -> [(Name, Type)] -> Context
 ctxdiff' ctx [] = ctx
-ctxdiff' ctx ((x, ty@(Type Unres _) : ps)) =
+ctxdiff' ctx ((x, ty@(Type Unres _)) : ps) =
     let ctx' = ctx `ctxdiff'` ps
     in  M.delete x ctx'
-ctxdiff' ctx ((x, ty@(Type Linear _) : ps)) =
+ctxdiff' ctx ((x, ty@(Type Linear _)) : ps) =
     let ctx' = ctx `ctxdiff'` ps
     in  case M.lookup x ctx' of
             Just (Type Linear _) ->
